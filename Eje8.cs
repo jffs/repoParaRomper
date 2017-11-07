26f2433daccc59c16ad1c83a3c74245a4055897e
Juan Fernandez Sosa <juaampi.edlp@gmail.com>
2017-11-07 11:33:05 -0300

    commiteando

--- Eje8.cs
+++ Eje8.cs
@@ -1,17 +1,1 @@
-using System;
-class Program
-{	
-	//Para ejecutar con parametros ir a Ejecutar - > Ejecutar con -> Parámetros personalizados. 
-	public static void Main(string[] args)
-	{
-		Console.WriteLine ("Iniciando saludos");
-		if (args.Length != 0) {
-			for (int i = 0; i < args.Length; i++)				//Usando FOR
-				Console.WriteLine ("Holaaaa {0}", args [i]);
-			foreach (string s in args)							//Usando Foreach
-				Console.WriteLine ("Holaaaa {0}", s);
-			
-		} else
-			Console.WriteLine ("No se recibieron parametros");
-}
-}
+nuevo contenido
