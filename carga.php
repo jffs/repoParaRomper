1f8c5c851f189bf60a49228f765be6874de55ec2
Juan Fernandez Sosa <juaampi.edlp@gmail.com>
2017-11-07 12:47:58 -0300

    commit

--- carga.php
+++ carga.php
@@ -1,7 +1,3 @@
-
-  if( ( $uploaded_type == "image/jpeg" || $uploaded_type == "image/png" )) { 
-  $target_path  = "./imagenes/"; //Directorio donde se almacenarán las imagenes
-  $target_path .= basename( $_FILES[ 'foto' ][ 'name' ] ); 
   move_uploaded_file($_FILES['foto']['tmp_name'],$target_path);
   echo "La foto fue subida con exito!<br>";
 
