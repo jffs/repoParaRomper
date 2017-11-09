97c2540016136a44faee52ad7ef0563c36b8ef9a
Juan Fernandez Sosa <juaampi.edlp@gmail.com>
2017-11-09 12:25:31 -0300

    commit

--- index.php
+++ index.php
@@ -5,11 +5,6 @@
 <body>
  
  
-<h1>Subir recuerdo de tu viaje más reciente </h1>
- <br><br>
-    Ubicación: <input type=text name="nombre"><br><br>
-    Descripción: <input type=text name="apellido">
-<br><br>
     Foto: <input type=file name="foto">
 
 <br><br>
